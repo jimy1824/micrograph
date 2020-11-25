802cbe0240b207e594bb37da26add5afe4ca6e1b
jamshaidiqbal <jamshaid1824@gmail.com>
2020-11-26 00:21:20 +0500

    first commit

---
+++ custom_auth/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from custom_auth.helper.auth_helper import get_sign_in_url, get_token_from_code, store_token, store_user, \
+    remove_user_and_token, get_token
+from custom_auth.helper.graph_helper import get_user
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def sign_in(request):
+    # Get the sign-in URL
+    sign_in_url, state = get_sign_in_url()
+    # Save the expected state so we can validate in the callback
+    request.session['auth_state'] = state
+    # Redirect to the Azure sign-in page
+    return HttpResponseRedirect(sign_in_url)
+
+
+def sign_out(request):
+    # Clear out the user and token
+    remove_user_and_token(request)
+
+    return HttpResponseRedirect(reverse('home'))
+
+
+def callback(request):
+    # Get the state saved in session
+    expected_state = request.session.pop('auth_state', '')
+    # Make the token request
+    token = get_token_from_code(request.get_full_path(), expected_state)
+
+    # Get the user's profile
+    user = get_user(token)
+
+    # Save token and user
+    store_token(request, token)
+    store_user(request, user)
+
+    return HttpResponseRedirect(reverse('home'))
---
+++ home/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from custom_auth.helper.graph_helper import get_calendar_events
+from custom_auth.helper.auth_helper import get_token
+import dateutil.parser
+
+def home(request):
+    context = initialize_context(request)
+
+    return render(request, 'home.html', context)
+
+
+def initialize_context(request):
+    context = {}
+
+    # Check for any errors in the session
+    error = request.session.pop('flash_error', None)
+
+    if error != None:
+        context['errors'] = []
+        context['errors'].append(error)
+
+    # Check for user in the session
+    context['user'] = request.session.get('user', {'is_authenticated': False})
+    return context
+
+
+def calendar(request):
+  context = initialize_context(request)
+
+  token = get_token(request)
+
+  events = get_calendar_events(token)
+
+  if events:
+    # Convert the ISO 8601 date times to a datetime object
+    # This allows the Django template to format the value nicely
+    for event in events['value']:
+      event['start']['dateTime'] = dateutil.parser.parse(event['start']['dateTime'])
+      event['end']['dateTime'] = dateutil.parser.parse(event['end']['dateTime'])
+
+    context['events'] = events['value']
+
+  return render(request, 'calendar.html', context)
